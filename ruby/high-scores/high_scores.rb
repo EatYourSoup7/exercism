cd41a8f1ca02e30693e64f47475b277bb368e239
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-02-14 08:59:51 -0600

    initial high scores test

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,19 @@
+=begin
+Write your code for the 'High Scores' exercise in this file. Make the tests in
+`high_scores_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/high-scores` directory.
+=end
+
+# class HighScores
+#   def initialize(input)
+#     @scores = scores
+#   end
+
+#   def scores
+#     return self.scores
+#   end
+
+
+# end
